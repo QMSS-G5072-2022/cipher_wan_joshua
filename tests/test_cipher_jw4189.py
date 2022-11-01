a05c916ddaf4d45750fed3b8856b9bd472d94806
jwnjoshua <jw4189@columbia.edu>
2022-11-01 14:51:26 +0000

    Added working version of cipher function

--- tests/test_cipher_jw4189.py
+++ tests/test_cipher_jw4189.py
@@ -1,1 +1,11 @@
 from cipher_jw4189 import cipher_jw4189
+import pytest
+
+def test_cipher():
+    expected="Dssoh"
+    result=cipher_jw4189.cipher("Apple",3)
+    assert result == expected
+
+def test_cipher_with_shift_as_string():
+    with pytest.raises(AssertionError):
+        cipher_jw4189.cipher("Apple","two")
