c23d346ab53840fa261ac0d82494276be2ecc0cf
jwnjoshua <jw4189@columbia.edu>
2022-11-01 14:37:10 +0000

    Added working version of cipher function

---
+++ src/cipher_jw4189/cipher_jw4189.py
@@ -0,0 +1,29 @@
+def cipher(text, shift, encrypt=True):
+    """"
+    The Caesar Cipher is one of the simplest and most widely known encryption techniques. 
+    In short, each letter is replaced by a letter some fixed number of positions down the alphabet. 
+    Apparently, Julius Caesar used it in his private correspondence.
+
+    Parameters
+    -----------
+    text : Text to be encrypted
+    shift : fixed number of positions down the alphabet
+    encrypt : takes in boolean values, encrypt = True to encrypt
+    Returns encrypted or decrypted text
+    --------
+    Examples
+    ---------
+    >>> cipher('Apple', 5)
+    'fuuqj'
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
