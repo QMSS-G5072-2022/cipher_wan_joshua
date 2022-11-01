e9c187154e289f6a4f612929139ff789780632e6
jwnjoshua <jw4189@columbia.edu>
2022-11-01 15:21:19 +0000

    Added working version of cipher function

--- tests/test_cipher_jw4189.py
+++ tests/test_cipher_jw4189.py
@@ -1,11 +1,16 @@
 from cipher_jw4189 import cipher_jw4189
 import pytest
 
-def test_cipher():
-    expected="Dssoh"
-    result=cipher_jw4189.cipher("Apple",3)
-    assert result == expected
+@pytest.fixture
+def example_text():
+    sample_shift_3=[ ("Apple","Dssoh"),
+                     ("apple","dssoh"),
+                     ("APPLE","DSSOH"),
+                     ("Mary lives in New York.","PduB olyhv lq Qhz brun.")
+                   ]
+    return sample_shift_3
 
-def test_cipher_with_shift_as_string():
-    with pytest.raises(AssertionError):
-        cipher_jw4189.cipher("Apple","two")
+@pytest.mark.parametrize("sample_text_index", [0,1,2,3])
+def test_cipher_expanded(example_text, sample_text_index): #example_text is a list of tuples
+    result=cipher_jw4189.cipher(example_text[sample_text_index][0], 3)
+    assert result == example_text[sample_text_index][1]
